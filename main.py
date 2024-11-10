a784eec828203397e2b179cc60869643120019eb
Franco Valledor <francovalledor@gmail.com>
2024-11-10 13:34:58 -0300

    create sequential algorithm

---
+++ main.py
@@ -0,0 +1,84 @@
+from os import makedirs
+from PIL import Image
+from numpy import number
+from utils import timer, BinayOperation
+
+OUTPUT_DIR = "result"
+
+VIDEO_DURATION = 4
+FRAMES_PER_SECOND = 24
+TOTAL_FRAMES_COUNT = FRAMES_PER_SECOND * VIDEO_DURATION
+DIGITS_COUNT = len(str(TOTAL_FRAMES_COUNT))
+
+
+def get_frame_name(index: number):
+    padded_index = str(index).zfill(DIGITS_COUNT)
+
+    return f"{OUTPUT_DIR}/{padded_index}.jpg"
+
+
+def save_image(image: Image, index: int):
+    image.save(get_frame_name(index))
+
+
+def load_image(path):
+    try:
+        image = Image.open(path).convert("RGB")
+        return image
+    except Exception as e:
+        print(f"Error loading image: {e}")
+        return None
+
+
+def process_images(pixels1, pixels2, size, operation: BinayOperation):
+    result_image = Image.new("RGB", size)
+    result_pixels = result_image.load()
+
+    (width, height) = size
+
+    for i in range(width):
+        for j in range(height):
+            r1, g1, b1 = pixels1[i, j]
+            r2, g2, b2 = pixels2[i, j]
+            result_pixels[i, j] = (
+                operation(r1, r2),
+                operation(g1, g2),
+                operation(b1, b2),
+            )
+
+    return result_image
+
+
+def fade_operation(percent: float):
+    def operation(p1: int, p2: int) -> int:
+        return int(p1 * percent + p2 * (1 - percent))
+
+    return operation
+
+
+@timer
+def run():
+    image_path1 = "image1.jpg"
+    image_path2 = "image2.jpg"
+
+    image1 = load_image(image_path1)
+    image2 = load_image(image_path2)
+
+    if image1.size != image2.size:
+        print("Images must have the same size")
+        return None
+
+    pixels1 = image1.load()
+    pixels2 = image2.load()
+    size = image1.size
+
+    makedirs(OUTPUT_DIR, exist_ok=True)
+
+    for i in range(TOTAL_FRAMES_COUNT):
+        percent = i / TOTAL_FRAMES_COUNT
+
+        result_image = process_images(pixels1, pixels2, size, fade_operation(percent))
+        save_image(result_image, i)
+
+
+run()
