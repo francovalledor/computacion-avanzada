03bf0d7b37a9ec8d215d03dd116d308fdda75a06
Franco Valledor <francovalledor@gmail.com>
2024-11-15 17:35:15 -0300

    create create_video_from_images

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import time
 from typing import Callable
+import cv2
+import os
 
 
 def timer(func):
@@ -25,3 +27,31 @@ def pad_with_zeros(num: int, count: int):
 
 
 BinayOperation = Callable[[int, int], int]
+
+
+def create_video_from_images(directory: str, output_video: str, fps: int = 30):
+    images = sorted(
+        [img for img in os.listdir(directory) if img.lower().endswith(".jpg")]
+    )
+
+    if not images:
+        print("No images found.")
+        return
+
+    first_image_path = os.path.join(directory, images[0])
+    frame = cv2.imread(first_image_path)
+    height, width, layers = frame.shape
+
+    output_file_name = f"{directory}/{output_video}.mp4"
+
+    fourcc = cv2.VideoWriter_fourcc(*"avc1")
+    video = cv2.VideoWriter(output_file_name, fourcc, fps, (width, height))
+
+    for image in images:
+        img_path = os.path.join(directory, image)
+        frame = cv2.imread(img_path)
+
+        video.write(frame)
+
+    video.release()
+    print(f"Created video: '{output_file_name}'")
